f1b6e65766e937345a36885e04de2209765aeae4
megahomyak <g.megahomyak@gmail.com>
2024-10-10 20:30:07 +0500

    Showing the password now for manual checking. Don't wanna end up with an unrecoverable backup!

--- pack.bash
+++ pack.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 BACKUP_DIR=~/i
-read -s -p "Password: " password \
-&& echo \
+read -p "Password: " password \
 && tar -I 'zstd -19' -cf backup.tar.zst -C $BACKUP_DIR . \
 && gpg --batch --yes --passphrase "${password}" --symmetric --cipher-algo AES256 -o backup.tar.zst.gpg backup.tar.zst \
 && rm backup.tar.zst
